b61a5b0cecac63f86d4f42e1e1a1e9b4971c7da3
DmitrySlv <webmaestros@yandex.ru>
2022-10-13 13:49:29 +0400

    Обновление библиотеки Pix. Добавление новой зависимости в build.gradle библиотеки Pix и удаление старой. Удаление jCenter(). Рефактор кода в ImagePicker.kt. Т.к теперь бибилотека использует fragment вместо activity.

--- app/src/main/java/com/ds_create/storeads/activities/EditAdsActivity.kt
+++ app/src/main/java/com/ds_create/storeads/activities/EditAdsActivity.kt
@@ -18,7 +18,6 @@ import com.ds_create.storeads.fragments.ImageListFrag
 import com.ds_create.storeads.models.AdModel
 import com.ds_create.storeads.utils.CityHelper
 import com.ds_create.storeads.utils.ImagePicker
-import com.fxn.utility.PermUtil
 
 class EditAdsActivity : AppCompatActivity(), FragmentCloseInterface {
 
@@ -71,28 +70,6 @@ class EditAdsActivity : AppCompatActivity(), FragmentCloseInterface {
         edTitle.setText(ad.title)
         edPrice.setText(ad.price)
         edDescription.setText(ad.description)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
-
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   // ImagePicker.getOptions(this, 3, ImagePicker.REQUEST_CODE_GET_IMAGES)
-                } else {
-                    Toast.makeText(this,
-                        "Approve permissions to open Pix ImagePicker",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-                return
-            }
-        }
     }
 
     //OnClicks
--- app/src/main/java/com/ds_create/storeads/utils/ImagePicker.kt
+++ app/src/main/java/com/ds_create/storeads/utils/ImagePicker.kt
@@ -2,15 +2,19 @@ package com.ds_create.storeads.utils
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.ds_create.storeads.R
 import com.ds_create.storeads.activities.EditAdsActivity
-import com.fxn.pix.Options
-import com.fxn.pix.Pix
-import com.fxn.utility.PermUtil
+import io.ak1.pix.helpers.PixEventCallback
+import io.ak1.pix.helpers.addPixToActivity
+import io.ak1.pix.models.Mode
+import io.ak1.pix.models.Options
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,62 +25,71 @@ object ImagePicker {
     const val SINGLE_IMAGE_COUNTER = 1
     const val MULTI_IMAGE_COUNTER = 3
     const val OPTIONS = "options"
+    private const val PATH_IMAGE_PICKER = "/pix/images"
 
     private fun getOptions(imageCounter: Int): Options {
-        val options = Options.init()
-            .setCount(imageCounter)
-            .setFrontfacing(false)
-            .setMode(Options.Mode.Picture)
-            .setScreenOrientation(Options.SCREEN_ORIENTATION_PORTRAIT)
-            .setPath("/pix/images")
+        val options = Options().apply {
+            count = imageCounter
+            isFrontFacing = false
+            mode = Mode.Picture
+            path = PATH_IMAGE_PICKER
+        }
         return options
     }
 
     fun launcher(
         edAct: EditAdsActivity, launcher: ActivityResultLauncher<Intent>?, imageCounter: Int
     ) {
-        PermUtil.checkForCamaraWritePermissions(edAct) {
-            val intent = Intent(edAct, Pix::class.java).apply {
-                putExtra(OPTIONS, getOptions(imageCounter))
+        edAct.addPixToActivity(R.id.placeHolder, getOptions(imageCounter)) { result->
+            when (result.status) {
+                PixEventCallback.Status.SUCCESS -> {
+                   val fList = edAct.supportFragmentManager.fragments
+                    fList.forEach {
+                        if (it.isVisible) edAct.supportFragmentManager.beginTransaction()
+                            .remove(it).commit()
+                    }
+                }
+               PixEventCallback.Status.BACK_PRESSED -> {
+                   Toast.makeText(edAct, "BACK_PRESSED", Toast.LENGTH_SHORT).show()
+               }
             }
-            launcher?.launch(intent)
         }
     }
 
     fun getLauncherForMultiSelectImages(edAct: EditAdsActivity): ActivityResultLauncher<Intent> {
         return edAct.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 result: ActivityResult ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                if (result.data != null) {
-                    val returnValues = result.data?.getStringArrayListExtra(Pix.IMAGE_RESULTS)
-                    if (returnValues?.size!! > 1 && edAct.chooseImageFrag == null) {
-                        edAct.openChooseImageFrag(returnValues)
-                    } else if (returnValues.size == 1 && edAct.chooseImageFrag == null) {
-                        CoroutineScope(Dispatchers.Main).launch {
-                            edAct.binding.pBarLoad.visibility = View.VISIBLE
-                            val bitmapArray =
-                                ImageManager.imageResize(returnValues) as ArrayList<Bitmap>
-                            edAct.binding.pBarLoad.visibility = View.GONE
-                            edAct.imageAdapter.update(bitmapArray)
-                        }
-                    } else if (edAct.chooseImageFrag != null) {
-                        edAct.chooseImageFrag?.updateAdapter(returnValues)
-                    }
-                }
-            }
+//            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+//                if (result.data != null) {
+//                    val returnValues = result.data?.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+//                    if (returnValues?.size!! > 1 && edAct.chooseImageFrag == null) {
+//                        edAct.openChooseImageFrag(returnValues)
+//                    } else if (returnValues.size == 1 && edAct.chooseImageFrag == null) {
+//                        CoroutineScope(Dispatchers.Main).launch {
+//                            edAct.binding.pBarLoad.visibility = View.VISIBLE
+//                            val bitmapArray =
+//                                ImageManager.imageResize(returnValues) as ArrayList<Bitmap>
+//                            edAct.binding.pBarLoad.visibility = View.GONE
+//                            edAct.imageAdapter.update(bitmapArray)
+//                        }
+//                    } else if (edAct.chooseImageFrag != null) {
+//                        edAct.chooseImageFrag?.updateAdapter(returnValues)
+//                    }
+//                }
+//            }
         }
     }
 
     fun getLauncherForSingleImage(edAct: EditAdsActivity): ActivityResultLauncher<Intent> {
         return edAct.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 result: ActivityResult ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK
-            ) {
-                if (result.data != null) {
-                    val uris = result.data?.getStringArrayListExtra(Pix.IMAGE_RESULTS)
-                    edAct.chooseImageFrag?.setSingleImage(uris?.get(0)!!, edAct.editImagePos)
-                }
-            }
+//            if (result.resultCode == AppCompatActivity.RESULT_OK
+//            ) {
+//                if (result.data != null) {
+//                    val uris = result.data?.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+//                    edAct.chooseImageFrag?.setSingleImage(uris?.get(0)!!, edAct.editImagePos)
+//                }
+//            }
         }
     }
 }
