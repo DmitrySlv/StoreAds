5e849bc941d9ce66862b2c54dab208aad1f791e9
DmitrySlv <webmaestros@yandex.ru>
2022-11-02 21:59:25 +0400

    Встроенные покупки в приложении ч1 (Purchase). Добавление зависимости в build.gradle для BillingClient - возможность создать покупки внутри приложения. Создание класса BillingManager.kt в котором настраивается сам клиент private fun setUpBillingClient() и его слушатель private fun getPurchaseListener(): PurchasesUpdatedListener.

---
+++ app/src/main/java/com/ds_create/storeads/utils/BillingManager.kt
@@ -0,0 +1,27 @@
+package com.ds_create.storeads.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.PurchasesUpdatedListener
+
+class BillingManager(private val act: AppCompatActivity) {
+    private var billingClient: BillingClient? = null
+
+    init {}
+
+    private fun setUpBillingClient() {
+        billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener())
+            .enablePendingPurchases().build()
+    }
+
+    private fun getPurchaseListener(): PurchasesUpdatedListener {
+        return PurchasesUpdatedListener {
+            result, list ->
+            run {
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                    list?.get(0).let {}
+                }
+            }
+        }
+    }
+}
