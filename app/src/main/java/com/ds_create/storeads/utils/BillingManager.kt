57f39825ba95de3f207640290eae91c348ef600a
DmitrySlv <webmaestros@yandex.ru>
2022-11-03 17:10:32 +0400

    Встроенные покупки в приложении ч2. Создание функции private fun getItem() - в которой создается покупка с выбранным вариантом.

--- app/src/main/java/com/ds_create/storeads/utils/BillingManager.kt
+++ app/src/main/java/com/ds_create/storeads/utils/BillingManager.kt
@@ -2,12 +2,18 @@ package com.ds_create.storeads.utils
 
 import androidx.appcompat.app.AppCompatActivity
 import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.BillingResult
 import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.SkuDetailsParams
 
 class BillingManager(private val act: AppCompatActivity) {
     private var billingClient: BillingClient? = null
 
-    init {}
+    init {
+        setUpBillingClient()
+    }
 
     private fun setUpBillingClient() {
         billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener())
@@ -23,5 +29,42 @@ class BillingManager(private val act: AppCompatActivity) {
                 }
             }
         }
+    }
+
+    private fun getItem() {
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_ADS)
+        val skuDetails = SkuDetailsParams.newBuilder()
+        skuDetails.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        billingClient?.querySkuDetailsAsync(skuDetails.build()) {
+                result, list ->
+            run {
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                    if (!list.isNullOrEmpty()) {
+                        val billingFlowParams = BillingFlowParams
+                            .newBuilder().setSkuDetails(list[0]).build()
+                        billingClient?.launchBillingFlow(act, billingFlowParams)
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun startConnection() {
+        billingClient?.startConnection(object : BillingClientStateListener {
+
+            override fun onBillingServiceDisconnected() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onBillingSetupFinished(p0: BillingResult) {
+                getItem()
+            }
+        })
+    }
+
+    companion object {
+       private const val REMOVE_ADS = "remove_ads"
     }
 }
